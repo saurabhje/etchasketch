7537a4edb08bdc250e630323410d1a3730e9c05a
saurabhje <work.saurabh@outlook.com>
2022-11-29 23:14:30 +0530

    functionalities

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
-
-function gridmaker(){
-const number = prompt("No. of Boxes[1-64]");
-const num = (700-2.1*number)/number;
-const container = document.querySelector('.container');
-for(let i =0;i<number;i++){
-    const div = document.createElement('div');
-    div.classList.add('gridcontainer')
-    container.appendChild(div);
-    for(let j =0;j<number;j++){
-        const div1 = document.createElement('div');
-        div1.setAttribute('class','divclass');
-        div1.setAttribute('style',`height:${num}px; width:${num}px;`);
-        div.appendChild(div1);
+const container = document.querySelector(".container");
+function gridmaker() {
+    const number = prompt("No. of Boxes[1-64]");
+    const num = 750 / number;
+    for (let i = 0; i < number; i++) {
+        const div = document.createElement("div");
+        div.classList.add("gridcontainer");
+        container.appendChild(div);
+        for (let j = 0; j < number; j++) {
+            const div1 = document.createElement("div");
+            div1.setAttribute("class", "divclass");
+            div1.setAttribute("style", `height:${num}px; width:${num}px;`);
+            div.appendChild(div1);
+        }
     }
-}
 
-div2 = document.querySelectorAll('.divclass');
-div2.forEach(element => {
-    element.addEventListener("mouseover",()=>{
-        element.style.backgroundColor = "black";
+    div2 = document.querySelectorAll(".divclass");
+    div2.forEach((element) => {
+        let i = 0;
+        let a = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        let c = Math.floor(Math.random() * 256);
+        element.addEventListener("mouseenter", () => {
+            element.style.background =
+                "rgb(" +
+                (a - (a / 2) * i) +
+                "," +
+                (b - (b / 3) * i) +
+                "," +
+                (c - (c / 5) * i) +
+                ")";
+            i++;
+        });
     });
-});
 }
 gridmaker();
+function gridset() {
+    while (container.firstChild) {
+        while (container.firstChild.firstChild) {
+            container.firstChild.removeChild(container.firstChild.firstChild);
+        }
+        container.removeChild(container.firstChild);
+    }
+    gridmaker();
+}
+
+let btn1 = document.querySelector(".btn");
+btn1.addEventListener("click", gridset);
